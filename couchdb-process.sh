f42a5b45c1d05b290639713e1d2b170c2348266f
Felix Schauerte <schauerte@sitewaerts.de>
2018-06-19 14:48:47 +0200

    sname --> name
    https://github.com/apache/couchdb/commit/e533ea057db2cf64d034da041f7f8704526a0b10

--- couchdb-process.sh
+++ couchdb-process.sh
@@ -17,9 +17,9 @@ fi
 if [ ! -z "$NODENAME" ] && ! grep "couchdb@" /home/couchdb/couchdb/etc/vm.args; then
   # A cookie is needed so that the nodes can connect to each other using Erlang clustering
   if [ -z "$COUCHDB_COOKIE" ]; then
-    echo "-sname couchdb@$NODENAME" >> /home/couchdb/couchdb/etc/vm.args
+    echo "-name couchdb@$NODENAME" >> /home/couchdb/couchdb/etc/vm.args
   else
-    echo "-sname couchdb@$NODENAME -setcookie '$COUCHDB_COOKIE'" >> /home/couchdb/couchdb/etc/vm.args
+    echo "-name couchdb@$NODENAME -setcookie '$COUCHDB_COOKIE'" >> /home/couchdb/couchdb/etc/vm.args
   fi
 fi
 
